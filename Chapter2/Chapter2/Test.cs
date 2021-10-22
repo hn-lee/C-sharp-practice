64d0e2dcc154e6e28d3616a2c19bc2906c52b42e
Sky Lee <belowalways@gmail.com>
2021-10-22 14:42:29 +0900

    Chapter2-commit-test
    Chapter2-commit-test

---
+++ Chapter2/Chapter2/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chapter2
+{
+    class Test
+    {
+        static void Main() // Method declaration
+        {
+            int x = 12 * 30; // Statement 1
+            Console.WriteLine(x); // Statement 2
+        } // End of method
+    }
+}
